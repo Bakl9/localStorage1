1ab6b5590ef80ee475476e38e5a071607201205e
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-06-04 16:22:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,32 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('h1').textContent = text
 };
 });
+
+
+
+document.querySelector('button').addEventListener('click', function () {
+  var value = document.querySelector('input').value
+
+  var obj = {
+    text: value
+  };
+
+  localStorage.setItem('headerText', JSON.stringify(obj));
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  var obj
+  try {
+    obj = JSON.parse(localStorage.getItem('headerText'));
+  } catch(e) {
+    obj = {}
+  };
+
+
+if(obj && obj.text && obj.text.trim()) {
+  document.querySelector('h1').textContent = obj.text
+};
+  
+
+});
